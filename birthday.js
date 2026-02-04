06dccd1ce227d32bbdb1fad5fdebbabd1230b73d
Mohit wadhwani <croma.laxmielectronics@gmail.com>
2026-02-04 20:25:07 +0530

    Update birthday.js

--- birthday.js
+++ birthday.js
@@ -16,7 +16,7 @@ client.on("ready", async () => {
 
   const sheets = google.sheets({ version: "v4", auth });
 
-  const sheetId = "https://docs.google.com/spreadsheets/d/1p-rLwjGcLZnt3lx6914Uyxcy0cjZMFxKU_lhjqvnZb4/edit?usp=sharing";
+  const sheetId = "1p-rLwjGcLZnt3lx6914Uyxcy0cjZMFxKU_lhjqvnZb4g";
 
   const res = await sheets.spreadsheets.values.get({
     spreadsheetId: sheetId,
